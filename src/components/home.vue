e8e074a613f0a32321a81e4804eb3a868c7dc01d
ddpicc <cuiqian1988@gmail.com>
2018-07-29 15:11:40 +0800

    add dashboard and menu

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,55 +1,171 @@
 <template>
+    <div>
+        <el-container class="main">
+            <el-aside :width="tabWidth+'px'">
+                <div>
+                    <div class="isClossTab" @click="isClossTabFun">
+                        <i :class="isCollapse?'el-icon-d-arrow-right':'el-icon-d-arrow-left'" ></i>
+                    </div>
 
-<el-container>
-    <el-header>
-<el-menu default-active="1-2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
-  <el-submenu index="1">
-    <template slot="title">
-      <i class="el-icon-menu"></i>
-      <span slot="title">导航一</span>
-    </template>
-    <el-menu-item-group>
-      <el-menu-item index="1-1">选项1</el-menu-item>
-      <el-menu-item index="1-2">选项2</el-menu-item>
-    </el-menu-item-group>
-  </el-submenu>
-</el-menu>
-<el-switch
-  v-model="isCollapse"
-  active-color="#13ce66"
-  inactive-color="#ff4949">
-</el-switch>
-    </el-header>
-    <el-main>
-        sdasda
-        <transition name="fade" mode="out-in">
-            <router-view></router-view>
-        </transition>
-    </el-main>
-</el-container>
+                    <el-menu :class="'menu'" :default-active="defaultActiveIndex" router :collapse="isCollapse" @select="handleSelect">
+                        <el-menu-item index="/Dashboard">
+                            <i class="el-icon-date"></i>
+                            <span slot="title">首页</span>
+                        </el-menu-item>
+                        <el-menu-item index="/Order">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title">生成订单</span>
+                        </el-menu-item>
+                        <el-menu-item index="/DataOper">
+                            <i class="el-icon-tickets"></i>
+                            <span slot="title">药品管理</span>
+                        </el-menu-item>                        
+                        <el-menu-item index="/OrderManage">
+                            <i class="el-icon-setting"></i>
+                            <span slot="title">订单管理</span>
+                        </el-menu-item>
+                    </el-menu>
+                </div>
+            </el-aside>
+            <el-container>
+                <el-header class="main-header">
+                    <el-dropdown>
+                        <span class="el-dropdown-link">
+                            <img src="" alt="">
+                        </span>
+                        <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item>退出登录</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                </el-header>
+                <el-main>
+                    <div>
+                        <router-view></router-view>
+                    </div>
+                </el-main>
+                <el-footer class="main-footer" height="50px">
+                    <p>footer</p>
+                </el-footer>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
+<script>
+    export default {
+        data() {
+            return {
+                isCollapse: false,
+                tabWidth: 150,
+                intelval: null,
+                defaultActiveIndex: "/",
+            };
+        },
+        methods: {
+            handleOpen(key, keyPath) {
+                console.log(key, keyPath);
+            },
+            handleClose(key, keyPath) {
+                console.log(key, keyPath);
+            },
+            handleSelect(index){
+                this.defaultActiveIndex = index;
+            },
+            isClossTabFun(){
+                clearInterval(this.intelval);
+                if(!this.isCollapse){
+                    this.intelval = setInterval(()=>{
+                        if(this.tabWidth<= 64)
+                            clearInterval(this.intelval);
+                        this.tabWidth -= 1;
+                    }, 1);
+                }else{
+                    this.tabWidth = 150;
+                }
+                this.isCollapse = !this.isCollapse;
+            }
+        }
+    }
+</script>
 <style>
-  .el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 200px;
-    min-height: 400px;
-  }
+    *{
+        padding: 0;
+        margin: 0;
+    }
+
+</style>
+<style scoped lang="scss">
+
+    $header-height:60px;
+    $background-color: #29aa99;
+    $color: #FFF;
+
+    .main{
+        height: calc(100vh - 60px);
+        min-width: 800px;
+        min-height: 600px;
+        overflow: hidden;
+
+        aside{
+            overflow: visible;
+            height: 100%;
+            background-color: $background-color;
+            color: #FFF;
+
+            .isClossTab{
+                width: 100%;
+                height: $header-height;
+                cursor: pointer;
+                font-size: 25px;
+                text-align: center;
+                line-height: $header-height;
+                font-weight: bold;
+                border-right: 1px solid #807c7c;
+                box-sizing: border-box;
+            }
+            .menu {
+                width: 100%;
+                border-right:0;
+
+
+            }
+
+        }
+
+        .main-header {
+            background-color: $background-color;
+            color: $color;
+
+            .el-dropdown{
+                cursor: pointer;
+                float: right;
+            }
+            .el-dropdown-link{
+
+                img{
+                    $imgMargin: (($header-height - 50)/2);
+                    display:inline-block;
+                    width:50px;
+                    height: 50px;
+                    border-radius: 25px;
+                    background-color: #FFF;
+                    margin-top: $imgMargin;
+                }
+            }
+        }
+
+        .crumbs {
+            margin-bottom: 20px;
+        }
+
+        .main-footer{
+            text-align: center;
+            background-color: $background-color;
+            color: $color;
+            line-height: 50px;
+        }
+
+    }
+
 </style>
 
-<script>
-  export default {
-    data() {
-      return {
-        isCollapse: true
-      };
-    },
-    methods: {
-      handleOpen(key, keyPath) {
-        console.log(key, keyPath);
-      },
-      handleClose(key, keyPath) {
-        console.log(key, keyPath);
-      }
-    }
-  }
-</script>
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,8 @@ import Detail from '@/components/Detail'
 import Order from '@/components/Order'
 import Home from '@/components/home'
 import OrderManage from '@/components/OrderManage'
+import Login from '@/components/Login'
+import Dashboard from '@/components/Dashboard'
 
 
 Vue.use(Router)
@@ -15,13 +17,29 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Home',
+      component: Home,
+      redirect: '/Dashboard',
+      leaf: true,
+      menuShow: true,
+      children: [
+        {path: '/Dashboard', component: Dashboard, name: '首页', menuShow: true}
+      ]
+    },
+    {
+      path: '/',
+      component: Home,
       name: 'Order',
-      component: Order
+      menuShow: true,
+      leaf: true, // 只有一个节点
+      children: [
+        {path: '/Order', component: Order, name: '生成订单', menuShow: true}
+      ]
     },
     {
       path: '/Login',
-      name: 'Home',
-      component: Home
+      name: 'Login',
+      component: Login,
     },
     {
       path : '/league/:name',
@@ -29,14 +47,24 @@ export default new Router({
       component : Detail
     },
     {
-      path : '/DataOper',
-      name : 'DataOper',
-      component : List
+      path: '/',
+      component: Home,
+      name: '药品管理',
+      menuShow: true,
+      leaf: true, // 只有一个节点
+      children: [
+        {path: '/DataOper', component: List, name: '药品列表', menuShow: true}
+      ]
     },
     {
-      path : '/Order',
-      name : 'OrderManage',
-      component : OrderManage
+      path: '/',
+      component: Home,
+      name: '订单管理',
+      menuShow: true,
+      leaf: true, // 只有一个节点
+      children: [
+        {path: '/OrderManage', component: OrderManage, name: '订单列表', menuShow: true}
+      ]
     }
   ]
 })
